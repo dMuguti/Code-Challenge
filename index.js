f7f0f40841076b89aeec72e07428af913bc07afd
dMuguti <omiahderek56@gmail.com>
2023-03-20 07:17:48 +0300

    Complete

--- index.js
+++ index.js
@@ -35,20 +35,18 @@ if (speed < 70) {  // check if the speed is less than 70 km/h
 
 function mySalaryFunction(){
 
-
-// Define the KRA, NHIF, and NSSF rates
 const KRA_RATE = 0.3;
 const NHIF_RATE = 0.015;
 const NSSF_RATE = 0.06;
 
-// Get the user's inputs
+
 const basicSalary = parseInt(document.getElementById("basic_salary").value);
 const benefits = parseInt(document.getElementById("benefits").value);
 
-// Calculate the gross salary
+
 const grossSalary = basicSalary + benefits;
 
-// Calculate the payee (tax) based on the KRA rates
+// Tax based on the KRA rates
 let payee = 0;
 if (grossSalary <= 24000) {
   payee = 0;
@@ -58,22 +56,18 @@ if (grossSalary <= 24000) {
   payee = (16000 * 0.1) + ((grossSalary - 40000) * 0.25);
 }
 
-// Calculate the NHIF deduction based on the NHIF rate
+//  NHIF deduction based on the NHIF rate
 const nhifDeduction = Math.min(grossSalary * NHIF_RATE, 1700);
 
-// Calculate the NSSF deduction based on the NSSF rate
+//  NSSF deduction based on the NSSF rate
 const nssfDeduction = Math.min(grossSalary * NSSF_RATE, 200);
 
-// Calculate the net salary
+//  net salary
 const netSalary = grossSalary - payee - nhifDeduction - nssfDeduction;
 
-// Print the results
+
 document.getElementById('salary_result').innerHTML = `Gross salary: KES ${grossSalary} <br> Payee (tax): KES ${payee} <br> NHIF deduction: KES ${nhifDeduction} <br> NSSF deduction: KES ${nssfDeduction} <br> Net salary: KES ${netSalary}`
-// console.log(`Gross salary: KES ${grossSalary.toFixed(2)}`);
-// console.log(`Payee (tax): KES ${payee.toFixed(2)}`);
-// console.log(`NHIF deduction: KES ${nhifDeduction.toFixed(2)}`);
-// console.log(`NSSF deduction: KES ${nssfDeduction.toFixed(2)}`);
-// console.log(`Net salary: KES ${netSalary.toFixed(2)}`);
+
 }
 
 
